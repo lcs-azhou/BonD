58bd588a84f2501b74f49b57d820c1bba20030a4
Ansheng Zhou <azhou@lcs.on.ca>
2024-05-24 13:12:13 -0400

    Initial Commit

---
+++ BonD/BonDApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonDApp.swift
+//  BonD
+//
+//  Created by Ansheng Zhou on 2024-05-24.
+//
+
+import SwiftUI
+
+@main
+struct BonDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
