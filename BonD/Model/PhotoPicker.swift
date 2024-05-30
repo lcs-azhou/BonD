5d07bbfe738180c844af33037e7acaca9adee86c
Ansheng Zhou <azhou@lcs.on.ca>
2024-05-30 12:02:01 -0400

    simplifying the functions

--- BonD/Model/PhotoPicker.swift
+++ BonD/Model/PhotoPicker.swift
@@ -10,42 +10,41 @@ import PhotosUI
 
 struct PhotoPicker: UIViewControllerRepresentable {
     @Binding var selectedImages: [UIImage]
-
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
-        var config = PHPickerConfiguration()
-        config.filter = .images
-        config.selectionLimit = 0
-        
-        let picker = PHPickerViewController(configuration: config)
+        let picker = PHPickerViewController(configuration: pickerConfiguration())
         picker.delegate = context.coordinator
         return picker
     }
-
-    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
-        // Perform any necessary update logic here
-    }
-
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
+    private func pickerConfiguration() -> PHPickerConfiguration {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 0
+        return config
+    }
+    
     class Coordinator: NSObject, PHPickerViewControllerDelegate {
         var parent: PhotoPicker
-
+        
         init(_ parent: PhotoPicker) {
             self.parent = parent
         }
-
+        
         func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
             picker.dismiss(animated: true)
-
+            
             for result in results {
-                if result.itemProvider.canLoadObject(ofClass: UIImage.self) {
-                    result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (image, error) in
-                        if let image = image as? UIImage {
-                            DispatchQueue.main.async {
-                                self?.parent.selectedImages.append(image)
-                            }
+                result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                    if let image = image as? UIImage {
+                        DispatchQueue.main.async {
+                            self?.parent.selectedImages.append(image)
                         }
                     }
                 }
