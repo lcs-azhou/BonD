bfad6818e15899000609d10890905f33ff6e4dba
Ansheng Zhou <azhou@lcs.on.ca>
2024-05-31 14:50:23 -0400

    Start up Itro tab page building

---
+++ BonD/Views/IntroItemView/IntroItem1.swift
@@ -0,0 +1,18 @@
+//
+//  IntroItem1.swift
+//  BonD
+//
+//  Created by Ansheng Zhou on 2024-05-31.
+//
+
+import SwiftUI
+
+struct IntroItem1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroItem1()
+}
---
+++ BonD/Views/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  BonD
+//
+//  Created by Ansheng Zhou on 2024-05-31.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
