b581532492fc5104051bc331e43dd46b539554f3
Ansheng Zhou <azhou@lcs.on.ca>
2024-06-06 20:14:55 -0400

    Adding Floating Window to timer function

---
+++ BonD/ViewModel/TimerViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TimerViewModel.swift
+//  BonD
+//
+//  Created by Ansheng Zhou on 2024-06-06.
+//
+
+import SwiftUI
+
+struct TimerViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerViewModel()
+}
---
+++ BonD/Views/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  BonD
+//
+//  Created by Ansheng Zhou on 2024-06-06.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
